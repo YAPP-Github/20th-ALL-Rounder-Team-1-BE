7407683aa111d8eea4deedda2932b5aa348c478d
limjimin <dkdldsla0000@naver.com>
2022-05-17 00:44:08 +0900

    :tada: 프로젝트 생성

---
+++ src/main/kotlin/com/yapp/weekand/WeekandApplication.kt
@@ -0,0 +1,11 @@
+package com.yapp.weekand
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WeekandApplication
+
+fun main(args: Array<String>) {
+    runApplication<WeekandApplication>(*args)
+}
