3ed4fc48af360b42269a2c5883e25ad8e138383e
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-06-02 20:10:19 +0900

    ✨ 닉네임 중복 확인 기능 추가 (#22)
    * :sparkles: 닉네임 중복 확인 기능 추가
    
    * :white_check_mark: 닉네임 중복 확인 테스트
    
    * Update src/main/resources/schema/schema.graphqls
    
    Co-authored-by: Jo Su Geun (조수근) <chris.jo@kakaostyle.com>
    
    Co-authored-by: Jo Su Geun (조수근) <chris.jo@kakaostyle.com>

---
+++ src/test/kotlin/com/yapp/weekand/domain/user/service/UserServiceTest.kt
@@ -0,0 +1,37 @@
+package com.yapp.weekand.domain.user.service
+
+import com.yapp.weekand.domain.user.entity.User
+import com.yapp.weekand.domain.user.repository.UserRepository
+import org.assertj.core.api.Assertions
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.transaction.annotation.Transactional
+
+@Transactional
+@SpringBootTest
+internal class UserServiceTest {
+	@Autowired
+	lateinit var userRepository: UserRepository
+
+	@Autowired
+	lateinit var userService: UserService
+
+	@Test
+	@DisplayName("닉네임 중복 확인을 한다.")
+	fun checkDuplicateNickname() {
+		// given
+		var user = User(email= "test@test.com",
+			nickname = "test",
+			password = "1234!",
+			marketingAgreed = true)
+
+		// when
+		userRepository.save(user)
+		val checkDuplicateNickname = userService.checkDuplicateNickname("test")
+
+		// then
+		Assertions.assertThat(checkDuplicateNickname).isEqualTo(true)
+	}
+}
