dc3b77ff390274524c69fe539b3fda9d8177520c
limjimin <dkdldsla0000@naver.com>
2022-06-07 00:58:33 +0900

    :wrench: securityconfig 수정

--- src/main/kotlin/com/yapp/weekand/common/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/yapp/weekand/common/config/security/SecurityConfig.kt
@@ -1,21 +1,42 @@
 package com.yapp.weekand.common.config.security
 
+import com.yapp.weekand.common.jwt.JwtAuthenticationFilter
+import com.yapp.weekand.common.jwt.JwtProvider
 import org.springframework.beans.factory.annotation.Configurable
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
 
 @Configurable
 @EnableWebSecurity
-class SecurityConfig : WebSecurityConfigurerAdapter() {
+class SecurityConfig(
+	private val jwtProvider: JwtProvider
+) : WebSecurityConfigurerAdapter() {
+	@Bean
+	fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
 
-    @Bean
-    fun encoder(): PasswordEncoder = BCryptPasswordEncoder()
+	override fun configure(web: WebSecurity) {
+		web
+			.ignoring().antMatchers("/api/v1/login", "/api/v1/signup", "/api/v1/refresh")
+	}
 
-    override fun configure(http: HttpSecurity?) {
-        http!!.csrf().disable().cors()
+    override fun configure(http: HttpSecurity) {
+		http
+			.httpBasic().disable()
+			.csrf().disable()
+			.httpBasic().disable()
+			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+			.and()
+			.authorizeRequests()
+			.antMatchers("/graphql").authenticated()
+			.antMatchers("/api/v1/signup", "/api/v1/login", "/api/v1/refresh").permitAll()
+			.and()
+			.addFilterBefore(JwtAuthenticationFilter(jwtProvider), UsernamePasswordAuthenticationFilter::class.java)
     }
 }
