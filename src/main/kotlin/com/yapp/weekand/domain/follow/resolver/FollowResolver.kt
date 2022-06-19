753b293183f03b07f7323e42e5edf2541539ce34
limjimin <dkdldsla0000@naver.com>
2022-06-20 00:13:10 +0900

    :recycle: 팔로우 resolver 리팩토링

--- src/main/kotlin/com/yapp/weekand/domain/follow/resolver/FollowResolver.kt
+++ src/main/kotlin/com/yapp/weekand/domain/follow/resolver/FollowResolver.kt
@@ -6,6 +6,7 @@ import com.yapp.weekand.domain.follow.dto.FollowDto
 import com.yapp.weekand.domain.follow.service.FollowService
 import com.yapp.weekand.domain.user.service.UserService
 import org.springframework.data.domain.PageRequest
+import org.springframework.security.access.prepost.PreAuthorize
 
 @DgsComponent
 class FollowResolver (
@@ -13,14 +14,16 @@ class FollowResolver (
 	private val userService: UserService
 ) {
 	@DgsQuery
+	@PreAuthorize("isAuthenticated()")
 	fun followers(page:Int, size:Int): FollowDto.FollowerList {
 		val followers = followService.getFollowers(userService.getCurrentUser(), PageRequest.of(page, size))
-		return FollowDto.FollowerList(FollowDto.PageInfo(followers.hasNext()), followers = followers.content)
+		return FollowDto.FollowerList(FollowDto.PaginationInfo(followers.hasNext()), followers = followers.content)
 	}
 
 	@DgsQuery
+	@PreAuthorize("isAuthenticated()")
 	fun followees(page:Int, size: Int): FollowDto.FolloweeList {
 		val followees = followService.getFollowees(userService.getCurrentUser(), PageRequest.of(page, size))
-		return FollowDto.FolloweeList(FollowDto.PageInfo(followees.hasNext()), followees = followees.content)
+		return FollowDto.FolloweeList(FollowDto.PaginationInfo(followees.hasNext()), followees = followees.content)
 	}
 }
