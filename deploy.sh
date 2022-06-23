3bbbff8dd145b7f5b6c707964994c40dcc23e891
sugeun.jo <whtnrms2012@gmail.com>
2022-06-23 23:22:21 +0900

    :rocket: 배포 스크립트 수정
    - build 시 test 태스크 제외
    - 코틀린 컴파일 이전에 dgs code gen 태스크 실행

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ echo "##########(ECR 에 로그인)##########"
 aws ecr get-login-password --region ap-northeast-2 --profile weekand-server |
 docker login --username AWS --password-stdin 256165122645.dkr.ecr.ap-northeast-2.amazonaws.com || exit 1
 echo "##########(Gradle build)##########"
-./gradlew clean build || exit 2
+./gradlew -x test clean build || exit 2
 echo "##########(이미지 build)##########"
 docker build --platform amd64 -t weekand-server . || exit 3
 echo "##########(이미지 tagging)##########"
