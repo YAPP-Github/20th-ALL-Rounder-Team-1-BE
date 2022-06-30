3fcd38dfa4b292bd56219b2831832b5f2b60d6cc
limjimin <dkdldsla0000@naver.com>
2022-06-30 19:07:10 +0900

    :recycle: 정규식 주석 추가

--- src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
+++ src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
@@ -4,6 +4,7 @@ import java.util.regex.Pattern
 
 object ValidationRegex {
 	private val EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
+	//숫자, 영어 조합 8자리 이상
 	private val PASSWORD_REGEX = Pattern.compile("""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$""")
 
 	fun isRegexEmail(email: String): Boolean {
