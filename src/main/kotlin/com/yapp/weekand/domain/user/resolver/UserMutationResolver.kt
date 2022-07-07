b0061a4e6cff95944d6c9b4aa38a6a0efc4e9812
sugeun.jo <whtnrms2012@gmail.com>
2022-07-07 23:14:25 +0900

    :sparkles: 사용자 프로필 수정 mutation 작성

--- src/main/kotlin/com/yapp/weekand/domain/user/resolver/UserMutationResolver.kt
+++ src/main/kotlin/com/yapp/weekand/domain/user/resolver/UserMutationResolver.kt
@@ -3,15 +3,22 @@ package com.yapp.weekand.domain.user.resolver
 import com.netflix.graphql.dgs.DgsComponent
 import com.netflix.graphql.dgs.DgsMutation
 import com.netflix.graphql.dgs.InputArgument
+import com.yapp.weekand.api.generated.types.UpdateUserProfileInput
 import com.yapp.weekand.domain.user.service.UserService
 
 @DgsComponent
-class UserMutationResolver (
+class UserMutationResolver(
 	private val userService: UserService
 ) {
 	@DgsMutation
 	fun inquiry(@InputArgument contents: String): Boolean {
 		userService.sendInquiryMail(userService.getCurrentUser(), contents)
+		return true
+	}
+
+	@DgsMutation
+	fun updateUserProfile(@InputArgument input: UpdateUserProfileInput): Boolean {
+		userService.updateUserProfile(userService.getCurrentUser().id, input)
 		return true
 	}
 }
