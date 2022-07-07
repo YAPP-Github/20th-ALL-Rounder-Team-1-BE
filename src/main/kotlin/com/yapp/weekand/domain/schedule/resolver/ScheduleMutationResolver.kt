c61bec581d649ded4dc61ccd426149db25afb39c
limjimin <dkdldsla0000@naver.com>
2022-07-07 18:44:10 +0900

    :sparkles: 스케줄 생성 mutation

---
+++ src/main/kotlin/com/yapp/weekand/domain/schedule/resolver/ScheduleMutationResolver.kt
@@ -0,0 +1,34 @@
+package com.yapp.weekand.domain.schedule.resolver
+
+import com.netflix.graphql.dgs.DgsComponent
+import com.netflix.graphql.dgs.DgsMutation
+import com.netflix.graphql.dgs.InputArgument
+import com.yapp.weekand.api.generated.types.ScheduleInput
+import com.yapp.weekand.common.util.Logger
+import com.yapp.weekand.domain.schedule.entity.RepeatType
+import com.yapp.weekand.domain.schedule.exception.ScheduleCreateException
+import com.yapp.weekand.domain.schedule.exception.ScheduleInvalidDateException
+import com.yapp.weekand.domain.schedule.service.ScheduleService
+import com.yapp.weekand.domain.user.service.UserService
+import org.springframework.security.access.prepost.PreAuthorize
+
+@DgsComponent
+class ScheduleMutationResolver (
+	private val scheduleService: ScheduleService,
+	private val userService: UserService
+) {
+	@DgsMutation
+	@PreAuthorize("isAuthenticated()")
+	fun createSchedule(@InputArgument input: ScheduleInput): Boolean {
+		if (input.dateTimeStart >= input.dateTimeEnd) {
+			throw ScheduleInvalidDateException()
+		}
+
+		if (input.repeatType == RepeatType.WEEKLY && input.repeatSelectedValue == null) {
+			throw ScheduleCreateException()
+		}
+
+		scheduleService.createSchedule(input, userService.getCurrentUser())
+		return true
+	}
+}
