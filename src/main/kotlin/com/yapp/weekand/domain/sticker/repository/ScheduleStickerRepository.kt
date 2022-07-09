dd249be427f7bfb262b849e4bd2bbc67bcc42830
limjimin <dkdldsla0000@naver.com>
2022-07-10 02:07:39 +0900

    :sparkles: 스티커 등록 로직

--- src/main/kotlin/com/yapp/weekand/domain/sticker/repository/ScheduleStickerRepository.kt
+++ src/main/kotlin/com/yapp/weekand/domain/sticker/repository/ScheduleStickerRepository.kt
@@ -2,9 +2,11 @@ package com.yapp.weekand.domain.sticker.repository
 
 import com.yapp.weekand.domain.schedule.entity.ScheduleRule
 import com.yapp.weekand.domain.sticker.entity.ScheduleSticker
+import com.yapp.weekand.domain.user.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
 import java.time.LocalDate
 
 interface ScheduleStickerRepository : JpaRepository<ScheduleSticker, Long> {
 	fun findByScheduleRuleAndScheduleDateOrderByDateCreatedDesc(scheduleRule: ScheduleRule, scheduleDate: LocalDate): List<ScheduleSticker>
+	fun findByUserAndScheduleRuleAndScheduleDate(user: User, scheduleRule: ScheduleRule, scheduleDate: LocalDate): ScheduleSticker?
 }
