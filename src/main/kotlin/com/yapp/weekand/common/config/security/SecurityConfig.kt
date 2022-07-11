5fdbf30385ccd9958947e5116abcf454c873d596
limjimin <dkdldsla0000@naver.com>
2022-07-11 17:51:17 +0900

    :recycle: filter 제거

--- src/main/kotlin/com/yapp/weekand/common/config/security/SecurityConfig.kt
+++ src/main/kotlin/com/yapp/weekand/common/config/security/SecurityConfig.kt
@@ -1,19 +1,15 @@
 package com.yapp.weekand.common.config.security
 
-import com.yapp.weekand.common.jwt.JwtAuthenticationFilter
 import com.yapp.weekand.common.jwt.JwtProvider
 import org.springframework.beans.factory.annotation.Configurable
 import org.springframework.context.annotation.Bean
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.builders.WebSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter
-import org.springframework.security.web.authentication.preauth.RequestHeaderAuthenticationFilter
 
 @Configurable
 @EnableWebSecurity
@@ -31,6 +27,5 @@ class SecurityConfig(
 			.antMatchers("/graphql","/graphiql").permitAll()
 			.and()
 			.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS).and()
-			.addFilterBefore(JwtAuthenticationFilter(jwtProvider), RequestHeaderAuthenticationFilter::class.java)
 	}
 }
