33a660c36c3eefcfe6a405df48d9e83f4f3008e0
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-07-16 16:20:19 +0900

    ✨ 카테고리 삭제 mutation 작성 (#121)
    * :sparkles: 카테고리 삭제 로직 작성
    
    * :sparkles: 카테고리 삭제 mutation 작성
    
    * :recycle: 대소문자 수정

--- src/main/kotlin/com/yapp/weekand/domain/schedule/repository/ScheduleRepository.kt
+++ src/main/kotlin/com/yapp/weekand/domain/schedule/repository/ScheduleRepository.kt
@@ -1,5 +1,6 @@
 package com.yapp.weekand.domain.schedule.repository
 
+import com.yapp.weekand.domain.category.entity.ScheduleCategory
 import com.yapp.weekand.domain.schedule.entity.ScheduleRule
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.data.jpa.repository.Query
@@ -8,12 +9,14 @@ import org.springframework.data.domain.Pageable
 import org.springframework.data.domain.Slice
 
 interface ScheduleRepository : JpaRepository<ScheduleRule, Long> {
-	@Query("SELECT r FROM ScheduleRule r join fetch r.scheduleCategory where r.scheduleCategory.id = :id and r.name like concat('%', :searchQuery, '%')")
+	@Query("SELECT r FROM ScheduleRule r JOIN FETCH r.scheduleCategory WHERE r.scheduleCategory.id = :id AND r.name LIKE CONCAT('%', :searchQuery, '%')")
 	fun searchScheduleRules(pageable: Pageable, @Param("searchQuery") searchQuery: String?, @Param("id") scheduleId: Long): Slice<ScheduleRule>
 
-	@Query("SELECT r FROM ScheduleRule r join fetch r.scheduleCategory where r.scheduleCategory.id = :id")
+	@Query("SELECT r FROM ScheduleRule r JOIN FETCH r.scheduleCategory WHERE r.scheduleCategory.id = :id")
 	fun findScheduleRules(pageable: Pageable, @Param("id") scheduleId: Long): Slice<ScheduleRule>
 
-	@Query("SELECT r FROM ScheduleRule r join fetch r.scheduleCategory left join fetch r.scheduleStatus where r.id = :id")
+	@Query("SELECT r FROM ScheduleRule r JOIN FETCH r.scheduleCategory LEFT JOIN FETCH r.scheduleStatus WHERE r.id = :id")
 	fun findByScheduleId(@Param("id") scheduleId: Long): ScheduleRule?
+
+	fun findByScheduleCategory(scheduleCategory: ScheduleCategory): List<ScheduleRule>
 }
