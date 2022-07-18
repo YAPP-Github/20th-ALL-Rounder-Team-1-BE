791b497ace6b3383814737573c7a79aa98c3305b
Jo Su Geun (조수근) <whtnrms2012@gmail.com>
2022-07-18 14:56:32 +0900

    :sparkles: CORS 설정 (#130)

---
+++ src/main/kotlin/com/yapp/weekand/common/config/security/WebConfig.kt
@@ -0,0 +1,14 @@
+package com.yapp.weekand.common.config.security
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.servlet.config.annotation.CorsRegistry
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+
+@Configuration
+class WebConfig : WebMvcConfigurer {
+	override fun addCorsMappings(registry: CorsRegistry) {
+		registry.addMapping("/**")
+			.allowedOrigins("*")
+			.allowedMethods("*")
+	}
+}
