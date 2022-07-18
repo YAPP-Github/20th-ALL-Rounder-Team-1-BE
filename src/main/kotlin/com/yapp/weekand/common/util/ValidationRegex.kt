80c277ed16095bc370a2cc385bee5a5b51532c45
limjimin <dkdldsla0000@naver.com>
2022-07-19 01:45:57 +0900

    :bug: 비밀번호 정규식 수정

--- src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
+++ src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
@@ -5,7 +5,7 @@ import java.util.regex.Pattern
 object ValidationRegex {
 	private val EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
 	//숫자, 영어 조합 8자리 이상
-	private val PASSWORD_REGEX = Pattern.compile("""^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$""")
+	private val PASSWORD_REGEX = Pattern.compile("^(?=.*\\d)(?=.*[a-z])[0-9a-zA-Z!@#${'$'}%^&*()\\-_=+{}|?>.<,:;~`']{8,}${'$'}")
 
 	fun isRegexEmail(email: String): Boolean {
 		return EMAIL_REGEX.matcher(email).matches()
