f33ae28a5a8353148c17c02a4f954c9b39a9d796
limjimin <dkdldsla0000@naver.com>
2022-07-20 13:44:07 +0900

    :adhesive_bandage: 비밀번호 정규식 주석 수정

--- src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
+++ src/main/kotlin/com/yapp/weekand/common/util/ValidationRegex.kt
@@ -4,7 +4,7 @@ import java.util.regex.Pattern
 
 object ValidationRegex {
 	private val EMAIL_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,6}$", Pattern.CASE_INSENSITIVE)
-	//숫자, 영어 조합 8자리 이상
+	//숫자, 영어 조합 8자리 이상, 특수문자 포함 가능
 	private val PASSWORD_REGEX = Pattern.compile("^(?=.*\\d)(?=.*[a-z])[0-9a-zA-Z!@#${'$'}%^&*()\\-_=+{}|?>.<,:;~`']{8,}${'$'}")
 
 	fun isRegexEmail(email: String): Boolean {
