a27c1909f64d0dd4354220eee7abba84fb24b9bd
Jimin Lim <50178026+jimin3263@users.noreply.github.com>
2022-07-21 14:53:37 +0900

    :rocket: 배포 스크립트 수정 (#149)

--- deploy.sh
+++ deploy.sh
@@ -1,12 +1,12 @@
 echo "##########(ECR 에 로그인)##########"
-aws ecr get-login-password --region ap-northeast-2 --profile weekand-server |
-docker login --username AWS --password-stdin 256165122645.dkr.ecr.ap-northeast-2.amazonaws.com || exit 1
+aws ecr-public get-login-password --region us-east-1 --profile weekand-server |
+docker login --username AWS --password-stdin public.ecr.aws/s4x9g1h9 || exit 1
 echo "##########(Gradle build)##########"
 ./gradlew -x test clean build || exit 2
 echo "##########(이미지 build)##########"
 docker build --platform amd64 -t weekand-server . || exit 3
 echo "##########(이미지 tagging)##########"
-docker tag weekand-server:latest 256165122645.dkr.ecr.ap-northeast-2.amazonaws.com/weekand-server:latest || exit 4
+docker tag weekand-server:latest public.ecr.aws/s4x9g1h9/weekand-server:latest || exit 4
 echo "##########(이미지 AWS ECR 에 push)##########"
-docker push 256165122645.dkr.ecr.ap-northeast-2.amazonaws.com/weekand-server:latest || exit 5
+docker push public.ecr.aws/s4x9g1h9/weekand-server:latest || exit 5
 echo "##########(이미지 배포 완료)##########"
